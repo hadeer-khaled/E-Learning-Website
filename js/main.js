b4fea9f8b84263c2073dc475ffaa88b391d6755a
Hadeer Khaled <hadeer.awad02@eng-st.cu.edu.eg>
2023-05-20 00:07:17 +0300

    dark mode

--- js/main.js
+++ js/main.js
@@ -9,3 +9,84 @@ function testimonialSider() {
     }
 }
 testimonialSider();
+
+// ---------------- Style Switcher ------------------ // 
+
+function styleSwitcherToggle() {
+    const styleSwitcher = document.querySelector(".js-style-switcher")
+    var styleSwitcherToggler = document.querySelector(".js-style-switcher-toggler")
+    styleSwitcherToggler.addEventListener("click", function() {
+        styleSwitcher.classList.toggle("open")
+        this.querySelector("i").classList.toggle("fa-times")
+        this.querySelector("i").classList.toggle("fa-cog")
+    })
+}
+styleSwitcherToggle()
+
+// ---------------- Color Themes ------------------ // 
+function colorThemes() {
+    const colorStyle = document.querySelector(".js-color-style");
+    var themeColorContainer = document.querySelector(".js-theme-colors")
+    themeColorContainer.addEventListener("click", ({target}) => {
+        if (target.classList.contains("js-theme-color-item")) {
+            // console.log(target.getAttribute("data-js-theme-color"))
+            localStorage.setItem("color", target.getAttribute("data-js-theme-color"))
+            setColor()
+        }
+    })
+    function setColor() {
+        let path = colorStyle.getAttribute("href").split("/")
+        path = path.slice(0, path.length - 1)
+        colorStyle.setAttribute("href" , path.join("/") + "/" + localStorage.getItem("color") + ".css")
+        // console.log(path)
+        if (document.querySelector(".js-theme-color-item.active")) {
+            document.querySelector(".js-theme-color-item.active").classList.remove("active")
+        }
+        document.querySelector("[data-js-theme-color=" + localStorage.getItem("color") +"]" ).classList.add("active")
+    }
+
+    if (localStorage.getItem("color") !== null) {
+        setColor()
+    }
+    else {
+        const defaultColor = colorStyle.getAttribute("href").split("/").pop().split(".").shift();
+        document.querySelector("[data-js-theme-color=" + defaultColor +"]" ).classList.add("active")
+    }
+}
+colorThemes()
+
+
+// ---------------- Dark Mode ------------------ // 
+function themeLightDark() {
+    const darkModeCheckBox = document.querySelector(".js-dark-mode")
+    darkModeCheckBox.addEventListener("click", function () {
+        if (this.checked) {
+            localStorage.setItem("dark-theme", "true");
+
+        }
+        else {
+            localStorage.setItem("dark-theme", "false");
+
+        }
+        themeMode()
+    });
+    function themeMode() {
+        if (localStorage.getItem("dark-theme") === "false") {
+            document.body.classList.remove("dark")
+        }
+        else {
+            document.body.classList.add("dark")
+            
+        }
+    }
+
+    if (localStorage.getItem("dark-theme") !== null) {
+        themeMode()   
+    }
+    if (document.body.classList.contains("dark")) {
+        darkModeCheckBox.checked = true;
+    }
+}
+themeLightDark() 
+
+// ---------------- Glass Effect ------------------ // 
